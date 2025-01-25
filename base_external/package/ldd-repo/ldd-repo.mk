2509323018370108144bebcbe7eb3a586ffaedd1
Oleksii Khovan <ahovan@gmail.com>
2025-01-25 22:56:31 +0100

    add more Buildroot-related stuff

--- base_external/package/ldd-repo/ldd-repo.mk
+++ base_external/package/ldd-repo/ldd-repo.mk
@@ -14,18 +14,7 @@ LDD_REPO_SITE = git@github.com:cu-ecen-aeld/assignment-7-ahovan.git
 LDD_REPO_SITE_METHOD = git
 LDD_REPO_GIT_SUBMODULES = YES
 
-define LDD_REPO_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
-endef
+LDD_REPO_MODULE_SUBDIRS = scull misc-modules
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define LDD_REPO_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
